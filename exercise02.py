65b763a4172bec8c44316e732dfa93fcd4ef5907
stevenshanecao <79949285+stevenshanecao@users.noreply.github.com>
2021-03-31 14:48:04 +1300

    Update exercise02.py

--- exercise02.py
+++ exercise02.py
@@ -1,4 +1,10 @@
 # Exercise Two
 # Write a simple program that finds the value at the nth position in the Fibonacci sequence
-
-print("The value at the 6th postion in the Fibonacci sequence is 8")
+def fibFinder(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        #xn = xn-1 + xn-2
+        return fibFinder(n-1) + fibFinder(n-2)
